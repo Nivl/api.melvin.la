8aef1bc096e77185b368eb7cb2e0b415021f35cc
Melvin Laplanche <melvin.laplanche+dev@gmail.com>
2016-05-11 19:31:25 -0700

    handle NoRoute

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"github.com/Nivl/api.melvin.la/app"
 	"github.com/Nivl/api.melvin.la/blog"
+	"github.com/Nivl/api.melvin.la/http-response"
 	"github.com/gin-gonic/gin"
 )
+
+func noRoute(gin *gin.Context) {
+	httpResponse.NotFound(gin)
+}
 
 func main() {
 	appContext := app.GetContext()
 	defer appContext.Destroy()
 
 	api := gin.Default()
+	api.NoRoute(noRoute)
 	blog.SetRoutes(api)
 	api.Run(":" + appContext.Params.Port)
 }
