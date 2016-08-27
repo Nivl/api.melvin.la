4b32f405eb1b3a9af2dac7adfae5ab03f93196c4
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-08-27 16:35:02 -0700

    Handle cors

--- src/main.go
+++ src/main.go
@@ -15,8 +15,25 @@ func ensureIndexes() {
 	blog.EnsureIndexes()
 }
 
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func start() {
 	api := gin.Default()
+	api.Use(corsMiddleware())
 	api.NoRoute(noRoute)
 	blog.SetRoutes(api)
 	api.Run(":" + app.GetContext().Params.Port)
