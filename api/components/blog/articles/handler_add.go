80c0c1722b9709ee0c0f6e07f69f26c4891aa65d
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-10-16 02:38:46 -0700

    [Request] Make endpoint.Params accessible from request.Params

--- api/components/blog/articles/handler_add.go
+++ api/components/blog/articles/handler_add.go
@@ -1,18 +1,23 @@
 package articles
 
-import "github.com/Nivl/api.melvin.la/api/router"
+import (
+	"github.com/Nivl/api.melvin.la/api/apierror"
+	"github.com/Nivl/api.melvin.la/api/router"
+)
 
 type HandlerAddParams struct {
-	Title       string `from:"query" json:"title" params:"required,trim"`
-	Subtitle    string `from:"query" json:"subtitle,omitempty"`
-	Description string `from:"query" json:"description,omitempty"`
-	Content     string `from:"query" json:"content,omitempty"`
+	Title       string `from:"form" json:"title,omitempty" params:"required,trim"`
+	Subtitle    string `from:"form" json:"subtitle,omitempty"`
+	Description string `from:"form" json:"description,omitempty"`
+	Content     string `from:"form" json:"content,omitempty"`
 }
 
 // HandlerAdd represents a API handler to add a new article
 func HandlerAdd(req *router.Request) {
-
-	params := req.Params.(HandlerAddParams)
+	params, ok := req.Params.(*HandlerAddParams)
+	if !ok {
+		req.Error(apierror.NewServerError("Couldn't cast params"))
+	}
 
 	a := &Article{
 		Title:       params.Title,
--- api/router/enpoints.go
+++ api/router/enpoints.go
@@ -19,7 +19,7 @@ func (endpoints Endpoints) Activate(router *mux.Router) {
 }
 
 // Handler makes it possible to use a RouteHandler where a http.Handler is required
-func Handler(endpoint *Endpoint) http.Handler {
+func Handler(e *Endpoint) http.Handler {
 	HTTPHandler := func(resWriter http.ResponseWriter, req *http.Request) {
 		request := &Request{
 			ID:       uuid.NewV4().String()[:8],
@@ -29,16 +29,22 @@ func Handler(endpoint *Endpoint) http.Handler {
 
 		request.Response.Header().Set("X-Request-Id", request.ID)
 
-		if err := endpoint.ParseParams(request); err != nil {
+		// TODO(melvin): It makes more sense to parse the params on the requests
+		// we need to use reflection to create the right instance of request.Params
+		// using the type of e.Params (which should only be holding a type)
+		// And then doing `request.ParseParseParams()`
+		// http://stackoverflow.com/questions/7850140/how-do-you-create-a-new-instance-of-a-struct-from-its-type-at-runtime-in-go
+		if err := e.ParseParams(request); err != nil {
 			request.Error(err)
 			return
 		}
+		request.Params = e.Params
 
 		defer request.handlePanic()
 
-		accessGranted := endpoint.Auth == nil || endpoint.Auth(request)
+		accessGranted := e.Auth == nil || e.Auth(request)
 		if accessGranted {
-			endpoint.Handler(request)
+			e.Handler(request)
 		}
 	}
 
