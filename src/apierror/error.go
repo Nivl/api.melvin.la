4cf37d2b02a5b3e435be9bd13e2df2d214a6083e
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-11-27 19:34:09 -0800

    [Error] Fix param passing

--- src/apierror/error.go
+++ src/apierror/error.go
@@ -33,18 +33,18 @@ func NewError(code int, message string, args ...interface{}) error {
 
 // NewServerError returns an Internal Error.
 func NewServerError(message string, args ...interface{}) error {
-	return NewError(http.StatusInternalServerError, message, args)
+	return NewError(http.StatusInternalServerError, message, args...)
 }
 
 // NewBadRequest returns an error caused by a user. Example: A missing param
 func NewBadRequest(message string, args ...interface{}) error {
-	return NewError(http.StatusBadRequest, message, args)
+	return NewError(http.StatusBadRequest, message, args...)
 }
 
 // NewConflict returns an error caused by a conflict with the current state
 // of the app. Example: A duplicate slug
 func NewConflict(message string, args ...interface{}) error {
-	return NewError(http.StatusConflict, message, args)
+	return NewError(http.StatusConflict, message, args...)
 }
 
 // NewUnauthorized returns an error caused by a anonymous user trying to access
