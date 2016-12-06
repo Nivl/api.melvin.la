4e82872ff05463a7d8515a0784e1a948b41b6b4a
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-12-05 23:31:03 -0800

    feat(router): Add cors

--- src/components/api/api.go
+++ src/components/api/api.go
@@ -7,6 +7,22 @@ import (
 	"github.com/melvin-laplanche/ml-api/src/components/users"
 )
 
+// AllowedOrigins is a list containing all origins allowed to hit the API
+var AllowedOrigins = []string{
+	"http://www.melvin.la",
+	"http://orchid.melvin.la",
+}
+
+// AllowedMethods is a list containing all HTTP verb accepted by the API
+var AllowedMethods = []string{
+	"GET", "POST", "PATCH", "DELETE",
+}
+
+// AllowedHeaders is a list custom headers accepted by the API
+var AllowedHeaders = []string{
+	"X-Session-Token", "X-User-Id",
+}
+
 // GetRouter return the api router with all the routes
 func GetRouter() *mux.Router {
 	baseURI := ""
