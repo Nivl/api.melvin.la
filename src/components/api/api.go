31a2f2dc35336f8c17418b3fe9f96cebba5860c5
Melvin Laplanche <Nivl@users.noreply.github.com>
2016-12-06 23:22:14 -0800

    fix(router): Return a JSON response for unexisting route

--- src/components/api/api.go
+++ src/components/api/api.go
@@ -1,11 +1,19 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/melvin-laplanche/ml-api/src/components/blog"
 	"github.com/melvin-laplanche/ml-api/src/components/sessions"
 	"github.com/melvin-laplanche/ml-api/src/components/users"
+	"github.com/melvin-laplanche/ml-api/src/mlhttp"
 )
+
+func notFound(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	mlhttp.ErrorJSON(w, `{"error":"not found"}`, http.StatusNotFound)
+}
 
 // GetRouter return the api router with all the routes
 func GetRouter() *mux.Router {
@@ -14,7 +22,7 @@ func GetRouter() *mux.Router {
 	blog.SetRoutes(baseURI, r)
 	users.SetRoutes(baseURI, r)
 	sessions.SetRoutes(baseURI, r)
-	//router.NotFoundHandler = http.HandlerFunc(noRoutes)
+	r.NotFoundHandler = http.HandlerFunc(notFound)
 
 	return r
 }
---
+++ src/components/api/api_test.go
@@ -0,0 +1,24 @@
+package api_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/melvin-laplanche/ml-api/src/components/api"
+	"github.com/stretchr/testify/assert"
+)
+
+// Test that an un-existing route returns JSON and a 404
+func TestRouteNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/404", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rec := httptest.NewRecorder()
+	api.GetRouter().ServeHTTP(rec, req)
+
+	assert.Equal(t, http.StatusNotFound, rec.Code)
+	assert.Equal(t, "application/json; charset=utf-8", rec.Header().Get("Content-Type"))
+}
