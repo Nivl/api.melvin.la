1c7f10f54c4b5a285f3cf31f8dc4bd2c3a30e120
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-04-02 19:58:34 -0700

    fix(main): Fix unexisting function

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import (
 	"net/http"
 
-	"github.com/Nivl/go-rest-tools/storage/db"
 	"github.com/gorilla/handlers"
 	"github.com/melvin-laplanche/ml-api/src/components/api"
 )
 
 func main() {
 	args := api.Setup()
-	defer db.Writer.Close()
 
 	r := api.GetRouter()
 	port := ":" + args.Port
