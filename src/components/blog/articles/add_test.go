8c3c8f8e7ef97bc4f0b7e24084b27d19d5e21ace
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-04-16 01:12:20 -0700

    chore(articles): Refactor tests for the add feature

--- src/components/blog/articles/add_test.go
+++ src/components/blog/articles/add_test.go
@@ -11,84 +11,149 @@ import (
 	"github.com/Nivl/go-rest-tools/security/auth/testdata"
 	"github.com/melvin-laplanche/ml-api/src/components/blog/articles"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestAdd(t *testing.T) {
-	globalT := t
+	testCases := []struct {
+		description string
+		f           func(t *testing.T)
+	}{
+		{"access data", addAccessData},
+		{"invalid title", addInvalidTitle},
+		{"valid data", addValid},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.description, tc.f)
+	}
+}
+
+func addInvalidTitle(t *testing.T) {
+	t.Parallel()
 	defer lifecycle.PurgeModels(t)
 
-	u1, s1 := testdata.NewAuth(t)
+	u, s := testdata.NewAuth(t)
+	auth := httptests.NewRequestAuth(s.ID, u.ID)
 
-	tests := []struct {
-		description string
-		code        int
-		params      *articles.AddParams
-		auth        *httptests.RequestAuth
-	}{
-		{
-			"No logged",
-			http.StatusBadRequest,
-			&articles.AddParams{},
-			nil,
-		},
-		{
-			"No Title",
-			http.StatusBadRequest,
-			&articles.AddParams{},
-			httptests.NewRequestAuth(s1.ID, u1.ID),
-		},
-		{
-			"Title filled with spaces",
-			http.StatusBadRequest,
-			&articles.AddParams{Title: "       "},
-			httptests.NewRequestAuth(s1.ID, u1.ID),
-		},
-		{
-			"As few params as possible",
-			http.StatusCreated,
-			&articles.AddParams{Title: "My Super Article"},
-			httptests.NewRequestAuth(s1.ID, u1.ID),
-		},
-		{
-			"Duplicate title",
-			http.StatusConflict,
-			&articles.AddParams{Title: "My Super Article"},
-			httptests.NewRequestAuth(s1.ID, u1.ID),
-		},
-		{
-			"All fields set",
-			http.StatusCreated,
-			&articles.AddParams{Title: "Title", Subtitle: "Subtitle", Description: "Description", Content: "Content"},
-			httptests.NewRequestAuth(s1.ID, u1.ID),
-		},
-	}
+	t.Run("parallel", func(t *testing.T) {
+		testCases := []struct {
+			description string
+			code        int
+			params      *articles.AddParams
+		}{
+			{
+				"No Title",
+				http.StatusBadRequest,
+				&articles.AddParams{},
+			},
+			{
+				"Title filled with spaces",
+				http.StatusBadRequest,
+				&articles.AddParams{Title: "       "},
+			},
+		}
 
-	for _, tc := range tests {
-		t.Run(tc.description, func(t *testing.T) {
-			rec := callAdd(t, tc.params, tc.auth)
-			assert.Equal(t, tc.code, rec.Code)
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.description, func(t *testing.T) {
+				t.Parallel()
 
-			if rec.Code == http.StatusCreated {
-				var a articles.Payload
-				if err := json.NewDecoder(rec.Body).Decode(&a); err != nil {
-					t.Fatal(err)
+				rec := callAdd(t, tc.params, auth)
+				assert.Equal(t, tc.code, rec.Code)
+			})
+		}
+	})
+}
+
+func addValid(t *testing.T) {
+	t.Parallel()
+	globalT := t
+	defer lifecycle.PurgeModels(globalT)
+
+	u, s := testdata.NewAuth(t)
+	auth := httptests.NewRequestAuth(s.ID, u.ID)
+
+	t.Run("parallel", func(t *testing.T) {
+		testCases := []struct {
+			description string
+			code        int
+			params      *articles.AddParams
+		}{
+			{
+				"As few params as possible",
+				http.StatusCreated,
+				&articles.AddParams{Title: "My Super Article"},
+			},
+			{
+				"All fields set",
+				http.StatusCreated,
+				&articles.AddParams{Title: "Title", Subtitle: "Subtitle", Description: "Description", Content: "Content"},
+			},
+		}
+
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.description, func(t *testing.T) {
+				t.Parallel()
+
+				rec := callAdd(t, tc.params, auth)
+				assert.Equal(t, tc.code, rec.Code)
+
+				if rec.Code == http.StatusCreated {
+					var a articles.Payload
+					if err := json.NewDecoder(rec.Body).Decode(&a); err != nil {
+						t.Fatal(err)
+					}
+
+					// Validate the article
+					assert.NotEmpty(t, a.ID)
+					assert.NotEmpty(t, a.Slug)
+					assert.Nil(t, a.PublishedAt)
+
+					// Validate the article's content
+					assert.Equal(t, tc.params.Title, a.Title)
+					assert.Equal(t, tc.params.Subtitle, a.Subtitle)
+					assert.Equal(t, tc.params.Description, a.Description)
+					assert.Equal(t, tc.params.Content, a.Content)
+
+					lifecycle.SaveModels(globalT, &articles.Article{ID: a.ID})
 				}
+			})
+		}
+	})
+}
 
-				// Validate the article
-				assert.NotEmpty(t, a.ID)
-				assert.NotEmpty(t, a.Slug)
-				assert.Nil(t, a.PublishedAt)
+// addAccessData tests that some wrong high level data are failing
+func addAccessData(t *testing.T) {
+	t.Parallel()
+	defer lifecycle.PurgeModels(t)
 
-				// Validate the article's content
-				assert.Equal(t, tc.params.Title, a.Title)
-				assert.Equal(t, tc.params.Subtitle, a.Subtitle)
-				assert.Equal(t, tc.params.Description, a.Description)
-				assert.Equal(t, tc.params.Content, a.Content)
+	t.Run("parallel", func(t *testing.T) {
+		testCases := []struct {
+			description string
+			code        int
+			auth        *httptests.RequestAuth
+			params      *articles.AddParams
+		}{
+			{
+				"As anonymous",
+				http.StatusUnauthorized,
+				nil,
+				&articles.AddParams{Title: "Title"},
+			},
+		}
 
-				lifecycle.SaveModels(globalT, &articles.Article{ID: a.ID})
-			}
-		})
-	}
+		for _, tc := range testCases {
+			tc := tc
+			t.Run(tc.description, func(t *testing.T) {
+				t.Parallel()
+				rec := callAdd(t, tc.params, tc.auth)
+				require.Equal(t, tc.code, rec.Code)
+			})
+		}
+	})
 }
 
 func callAdd(t *testing.T, params *articles.AddParams, auth *httptests.RequestAuth) *httptest.ResponseRecorder {
