a3e675b9cdac98c2c15f05c162982dbeb2dc691c
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-04-29 16:22:35 -0700

    Fix(ci): Add a 1 minute sleep before running goose

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -5,4 +5,5 @@ set -ex
 heroku plugins:install heroku-container-registry
 heroku container:login
 heroku container:push web --app melvin-laplanche
+sleep 1m # let's wait a minute that everything get setup
 heroku run "cd /go/src/github.com/melvin-laplanche/ml-api && goose up" --app melvin-laplanche
