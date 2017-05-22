86bc91f9e52fac506b20721a8449208e6787196f
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-05-21 21:36:38 -0700

    feat(user): Add IsAdmin to the user payload

--- src/components/users/payload.go
+++ src/components/users/payload.go
@@ -4,9 +4,10 @@ import "github.com/Nivl/go-rest-tools/security/auth"
 
 // Payload represents a user payload with non public field
 type Payload struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Email string `json:"email,omitempty"`
+	ID      string `json:"id"`
+	Name    string `json:"name"`
+	Email   string `json:"email,omitempty"`
+	IsAdmin bool   `json:"is_admin,omitempty"`
 }
 
 // NewPrivatePayload turns a user into an object that is safe to be
@@ -21,7 +22,8 @@ func NewPrivatePayload(u *auth.User) *Payload {
 // returned by the API
 func NewPayload(u *auth.User) *Payload {
 	return &Payload{
-		ID:   u.ID,
-		Name: u.Name,
+		ID:      u.ID,
+		Name:    u.Name,
+		IsAdmin: u.IsAdmin,
 	}
 }
