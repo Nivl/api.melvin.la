13bb85df6ff3087863679b5e2f5ab0093bc1edae
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-07-03 00:30:06 -0700

    tests: Add unit tests for users.Get

--- src/components/users/get.handler.go
+++ src/components/users/get.handler.go
@@ -8,12 +8,17 @@ import (
 
 // GetParams represent the request params accepted by HandlerGet
 type GetParams struct {
-	ID string `from:"url" json:"id" params:"uuid"`
+	ID string `from:"url" json:"id" params:"uuid,required"`
 }
 
 // Get represent an API handler to get a user
 func Get(req router.HTTPRequest, deps *router.Dependencies) error {
 	params := req.Params().(*GetParams)
+
+	// if a user asks for their own data, we don't need to query the DB
+	if req.User() != nil && req.User().ID == params.ID {
+		return req.Response().Ok(NewPrivatePayload(req.User()))
+	}
 
 	user, err := auth.GetUser(deps.DB, params.ID)
 	if err != nil {
@@ -23,9 +28,5 @@ func Get(req router.HTTPRequest, deps *router.Dependencies) error {
 		return httperr.NewNotFound()
 	}
 
-	// if a user asks for their own data, we return as much as possible
-	if req.User() != nil && req.User().ID == user.ID {
-		return req.Response().Ok(NewPrivatePayload(user))
-	}
 	return req.Response().Ok(NewPayload(user))
 }
---
+++ src/components/users/get.handler_test.go
@@ -0,0 +1,200 @@
+package users_test
+
+import (
+	"database/sql"
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/Nivl/go-rest-tools/network/http/httperr"
+	"github.com/Nivl/go-rest-tools/router"
+	"github.com/Nivl/go-rest-tools/router/mockrouter"
+	"github.com/Nivl/go-rest-tools/security/auth"
+	"github.com/Nivl/go-rest-tools/storage/db/mockdb"
+	"github.com/melvin-laplanche/ml-api/src/components/users"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetInvalidParams(t *testing.T) {
+	testCases := []struct {
+		description string
+		sources     map[string]url.Values
+	}{
+		{
+			"Should fail on missing ID",
+			map[string]url.Values{
+				"url": url.Values{},
+			},
+		},
+		{
+			"Should fail on invalid ID",
+			map[string]url.Values{
+				"url": url.Values{
+					"id": []string{"not-a-uuid"},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
+
+			endpts := users.Endpoints[users.EndpointGet]
+			_, err := endpts.Guard.ParseParams(tc.sources)
+			assert.Error(t, err)
+		})
+	}
+}
+
+func TestGetValidParams(t *testing.T) {
+	testCases := []struct {
+		description string
+		sources     map[string]url.Values
+	}{
+		{
+			"Should fail on blank password",
+			map[string]url.Values{
+				"url": url.Values{
+					"id": []string{"48d0c8b8-d7a3-4855-9d90-29a06ef474b0"},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
+
+			endpts := users.Endpoints[users.EndpointGet]
+			data, err := endpts.Guard.ParseParams(tc.sources)
+			assert.NoError(t, err)
+
+			if data != nil {
+				p := data.(*users.GetParams)
+				assert.Equal(t, tc.sources["url"].Get("id"), p.ID)
+			}
+		})
+	}
+}
+
+func TestGetOthersData(t *testing.T) {
+	handlerParams := &users.GetParams{
+		ID: "0c2f0713-3f9b-4657-9cdd-2b4ed1f214e9",
+	}
+	requester := &auth.User{
+		ID: "48d0c8b8-d7a3-4855-9d90-29a06ef474b0",
+	}
+	userToGet := &auth.User{
+		ID:      handlerParams.ID,
+		Name:    "user name",
+		Email:   "email@domain.tld",
+		IsAdmin: false,
+	}
+
+	// Mock the database & add expectations
+	dbCon := new(mockdb.DB)
+	dbGetCall := dbCon.On("Get", mock.Anything, mock.AnythingOfType("string"), mock.Anything)
+	dbGetCall.Return(nil)
+	dbGetCall.Run(func(args mock.Arguments) {
+		user := args.Get(0).(*auth.User)
+		user.ID = userToGet.ID
+		user.Name = userToGet.Name
+		user.Email = userToGet.Email
+		user.IsAdmin = userToGet.IsAdmin
+	})
+
+	// Mock the response & add expectations
+	res := new(mockrouter.HTTPResponse)
+	res.On("Ok", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		pld := args.Get(0).(*users.Payload)
+		assert.Equal(t, userToGet.ID, pld.ID, "ID should have not changed")
+		assert.Equal(t, userToGet.Name, pld.Name, "Name should have not changed")
+		assert.Empty(t, pld.Email, "the email should not be returned to anyone")
+		assert.False(t, pld.IsAdmin, "user should not be an admin")
+	})
+
+	// Mock the request & add expectations
+	req := new(mockrouter.HTTPRequest)
+	req.On("Response").Return(res)
+	req.On("Params").Return(handlerParams)
+	req.On("User").Return(requester)
+
+	// call the handler
+	err := users.Get(req, &router.Dependencies{DB: dbCon})
+
+	// Assert everything
+	assert.NoError(t, err, "the handler should not have fail")
+	dbCon.AssertExpectations(t)
+	req.AssertExpectations(t)
+	res.AssertExpectations(t)
+}
+
+func TestGetOwnData(t *testing.T) {
+	handlerParams := &users.GetParams{
+		ID: "0c2f0713-3f9b-4657-9cdd-2b4ed1f214e9",
+	}
+	requester := &auth.User{
+		ID:      handlerParams.ID,
+		Name:    "user name",
+		Email:   "email@domain.tld",
+		IsAdmin: false,
+	}
+
+	// Mock the response & add expectations
+	res := new(mockrouter.HTTPResponse)
+	res.On("Ok", mock.Anything).Return(nil).Run(func(args mock.Arguments) {
+		pld := args.Get(0).(*users.Payload)
+		assert.Equal(t, requester.ID, pld.ID, "ID should have not changed")
+		assert.Equal(t, requester.Name, pld.Name, "Name should have not changed")
+		assert.Equal(t, requester.Email, pld.Email, "the email should be returned")
+		assert.False(t, pld.IsAdmin, "user should not be an admin")
+	})
+
+	// Mock the request & add expectations
+	req := new(mockrouter.HTTPRequest)
+	req.On("Response").Return(res)
+	req.On("Params").Return(handlerParams)
+	req.On("User").Return(requester)
+
+	// call the handler
+	err := users.Get(req, &router.Dependencies{})
+
+	// Assert everything
+	assert.NoError(t, err, "the handler should not have fail")
+	req.AssertExpectations(t)
+	res.AssertExpectations(t)
+}
+
+func TestGetUnexistingUser(t *testing.T) {
+	handlerParams := &users.GetParams{
+		ID: "0c2f0713-3f9b-4657-9cdd-2b4ed1f214e9",
+	}
+	requester := &auth.User{
+		ID: "48d0c8b8-d7a3-4855-9d90-29a06ef474b0",
+	}
+
+	// Mock the database & add expectations
+	dbCon := new(mockdb.DB)
+	dbGetCall := dbCon.On("Get", mock.Anything, mock.AnythingOfType("string"), mock.Anything)
+	dbGetCall.Return(sql.ErrNoRows)
+
+	// Mock the request & add expectations
+	req := new(mockrouter.HTTPRequest)
+	req.On("Params").Return(handlerParams)
+	req.On("User").Return(requester)
+
+	// call the handler
+	err := users.Get(req, &router.Dependencies{DB: dbCon})
+
+	// Assert everything
+	assert.Error(t, err, "the handler should have fail")
+	dbCon.AssertExpectations(t)
+	req.AssertExpectations(t)
+
+	httpErr := httperr.Convert(err)
+	assert.Equal(t, http.StatusNotFound, httpErr.Code())
+}
