e8ee8b726170d866d490feb8ec3e2d94c89c4dcd
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-07-20 01:10:29 -0700

    feat(organiation): Add endpoint to delete an organizaion

---
+++ src/components/about/organizations/delete.handler_test.go
@@ -0,0 +1,162 @@
+package organizations_test
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/Nivl/go-rest-tools/network/http/httperr"
+	"github.com/Nivl/go-rest-tools/router"
+	"github.com/Nivl/go-rest-tools/router/guard/testguard"
+	"github.com/Nivl/go-rest-tools/router/mockrouter"
+	"github.com/Nivl/go-rest-tools/security/auth"
+	"github.com/Nivl/go-rest-tools/storage/db/mockdb"
+	"github.com/melvin-laplanche/ml-api/src/components/about/organizations"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestDeleteInvalidParams(t *testing.T) {
+	testCases := []testguard.InvalidParamsTestCase{
+		{
+			Description: "Should fail on missing ID",
+			MsgMatch:    "parameter missing",
+			FieldName:   "id",
+			Sources: map[string]url.Values{
+				"url": url.Values{},
+				"form": url.Values{
+					"current_password": []string{"password"},
+				},
+			},
+		},
+		{
+			Description: "Should fail on invalid ID",
+			MsgMatch:    "not a valid uuid",
+			FieldName:   "id",
+			Sources: map[string]url.Values{
+				"url": url.Values{
+					"id": []string{"not-a-uuid"},
+				},
+				"form": url.Values{
+					"current_password": []string{"password"},
+				},
+			},
+		},
+	}
+
+	g := organizations.Endpoints[organizations.EndpointDelete].Guard
+	testguard.InvalidParams(t, g, testCases)
+}
+
+func TestDeleteValidParams(t *testing.T) {
+	testCases := []struct {
+		description string
+		sources     map[string]url.Values
+	}{
+		{
+			"Should fail on blank password",
+			map[string]url.Values{
+				"url": url.Values{
+					"id": []string{"48d0c8b8-d7a3-4855-9d90-29a06ef474b0"},
+				},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			t.Parallel()
+
+			endpts := organizations.Endpoints[organizations.EndpointDelete]
+			data, err := endpts.Guard.ParseParams(tc.sources, nil)
+			assert.NoError(t, err)
+
+			if data != nil {
+				p := data.(*organizations.DeleteParams)
+				assert.Equal(t, tc.sources["url"].Get("id"), p.ID)
+			}
+		})
+	}
+}
+
+func TestDeleteAccess(t *testing.T) {
+	testCases := []testguard.AccessTestCase{
+		{
+			Description: "Should fail for anonymous users",
+			User:        nil,
+			ErrCode:     http.StatusUnauthorized,
+		},
+		{
+			Description: "Should work for logged users",
+			User:        &auth.User{ID: "48d0c8b8-d7a3-4855-9d90-29a06ef474b0"},
+			ErrCode:     http.StatusForbidden,
+		},
+		{
+			Description: "Should work for logged users",
+			User:        &auth.User{ID: "48d0c8b8-d7a3-4855-9d90-29a06ef474b0", IsAdmin: true},
+			ErrCode:     0,
+		},
+	}
+
+	g := organizations.Endpoints[organizations.EndpointDelete].Guard
+	testguard.AccessTest(t, g, testCases)
+}
+
+func TestDeleteHappyPath(t *testing.T) {
+	handlerParams := &organizations.DeleteParams{
+		ID: "48d0c8b8-d7a3-4855-9d90-29a06ef474b0",
+	}
+
+	// Mock the database & add expectations
+	mockDB := new(mockdb.DB)
+	mockDB.ExpectDeletion()
+	mockDB.ExpectGet("*organizations.Organization", func(args mock.Arguments) {
+		org := args.Get(0).(*organizations.Organization)
+		org.ID = "aa44ca86-553e-4e16-8c30-2e50e63f7eaa"
+		org.Name = "Google"
+	})
+
+	// Mock the response & add expectations
+	res := new(mockrouter.HTTPResponse)
+	res.On("NoContent").Return()
+
+	// Mock the request & add expectations
+	req := new(mockrouter.HTTPRequest)
+	req.On("Response").Return(res)
+	req.On("Params").Return(handlerParams)
+
+	// call the handler
+	err := organizations.Delete(req, &router.Dependencies{DB: mockDB})
+
+	// Assert everything
+	assert.NoError(t, err, "the handler should not have fail")
+	mockDB.AssertExpectations(t)
+	req.AssertExpectations(t)
+	res.AssertExpectations(t)
+}
+
+func TestDeleteUnexistingOrganization(t *testing.T) {
+	handlerParams := &organizations.DeleteParams{
+		ID: "48d0c8b8-d7a3-4855-9d90-29a06ef474b0",
+	}
+
+	// Mock the database & add expectations
+	mockDB := new(mockdb.DB)
+	mockDB.ExpectGetNotFound("*organizations.Organization")
+
+	// Mock the request & add expectations
+	req := new(mockrouter.HTTPRequest)
+	req.On("Params").Return(handlerParams)
+
+	// call the handler
+	err := organizations.Delete(req, &router.Dependencies{DB: mockDB})
+
+	// Assert everything
+	assert.Error(t, err, "the handler should have fail")
+	req.AssertExpectations(t)
+	mockDB.AssertExpectations(t)
+
+	httpErr := httperr.Convert(err)
+	assert.Equal(t, http.StatusNotFound, httpErr.Code())
+}
