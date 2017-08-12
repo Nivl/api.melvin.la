2c27d6dbf065f05264071ab88fb66a6649141f41
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-08-11 22:09:17 -0700

    Add integration tests

---
+++ src/components/about/experience/get.handler_integration_test.go
@@ -0,0 +1,138 @@
+// +build integration
+
+package experience_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Nivl/go-rest-tools/dependencies"
+	"github.com/Nivl/go-rest-tools/network/http/httptests"
+	"github.com/Nivl/go-rest-tools/security/auth/testauth"
+	"github.com/Nivl/go-rest-tools/storage/db"
+	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
+	"github.com/melvin-laplanche/ml-api/src/components/about/experience"
+	"github.com/melvin-laplanche/ml-api/src/components/about/experience/testexperience"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIntegrationGet(t *testing.T) {
+	dbCon := dependencies.DB
+	defer lifecycle.PurgeModels(t, dbCon)
+
+	_, userSession := testauth.NewAuth(t, dbCon)
+	_, adminSession := testauth.NewAdminAuth(t, dbCon)
+
+	basicExp := testexperience.NewPersisted(t, dbCon, nil)
+
+	orphanExp := testexperience.NewPersisted(t, dbCon, nil)
+	orphanExp.Organization.DeletedAt = db.Now()
+	orphanExp.Organization.Update(dbCon)
+
+	tests := []struct {
+		description  string
+		code         int
+		params       *experience.GetParams
+		auth         *httptests.RequestAuth
+		expectedData *experience.Experience
+	}{
+		{
+			"Anonymous getting an orphan exp",
+			http.StatusNotFound,
+			&experience.GetParams{ID: orphanExp.ID},
+			nil,
+			orphanExp,
+		},
+		{
+			"User getting an orphan exp",
+			http.StatusNotFound,
+			&experience.GetParams{ID: orphanExp.ID},
+			httptests.NewRequestAuth(userSession),
+			orphanExp,
+		},
+		{
+			"Admin getting an orphan exp",
+			http.StatusOK,
+			&experience.GetParams{ID: orphanExp.ID},
+			httptests.NewRequestAuth(adminSession),
+			orphanExp,
+		},
+		{
+			"Anonymous getting a basic exp",
+			http.StatusOK,
+			&experience.GetParams{ID: basicExp.ID},
+			nil,
+			basicExp,
+		},
+		{
+			"Admin getting a basic exp",
+			http.StatusOK,
+			&experience.GetParams{ID: basicExp.ID},
+			httptests.NewRequestAuth(adminSession),
+			basicExp,
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			rec := callGet(t, tc.params, tc.auth)
+			assert.Equal(t, tc.code, rec.Code)
+
+			if rec.Code == http.StatusOK {
+				var pld experience.Payload
+				if err := json.NewDecoder(rec.Body).Decode(&pld); err != nil {
+					t.Fatal(err)
+				}
+
+				assert.Equal(t, tc.expectedData.ID, pld.ID, "ID should not have changed")
+				assert.Equal(t, tc.expectedData.JobTitle, pld.JobTitle, "JobTitle should not have changed")
+				assert.Equal(t, tc.expectedData.Location, pld.Location, "Location should not have changed")
+				assert.Equal(t, tc.expectedData.Description, pld.Description, "Description should not have changed")
+				assert.Equal(t, tc.expectedData.StartDate.String(), pld.StartDate.String(), "StartDate should not have changed")
+				if tc.expectedData.EndDate != nil {
+					assert.Equal(t, tc.expectedData.EndDate.String(), pld.EndDate.String(), "EndDate should not have changed")
+				} else {
+					assert.Nil(t, tc.expectedData.EndDate, "EndDate should still be nil")
+				}
+
+				// we assume that if we have one field, we have all the fields
+				assert.NotNil(t, pld.Organization, "Organization should not have been nil")
+				assert.Equal(t, tc.expectedData.Organization.ID, pld.Organization.ID, "Organization.ID should not have changed")
+
+				if tc.auth != nil && tc.auth.SessionID == adminSession.ID {
+					assert.NotNil(t, pld.CreatedAt, "CreatedAt should have been set")
+					assert.NotNil(t, pld.UpdatedAt, "UpdatedAt should have been set")
+					assert.NotNil(t, pld.Organization.CreatedAt, "Organization.CreatedAt should have been set")
+					assert.NotNil(t, pld.Organization.UpdatedAt, "Organization.UpdatedAt should have been set")
+
+					if tc.expectedData.DeletedAt != nil {
+						assert.NotNil(t, pld.DeletedAt, "DeletedAt should have been set")
+					}
+
+					if tc.expectedData.Organization.DeletedAt != nil {
+						assert.NotNil(t, pld.Organization.DeletedAt, "Organization.DeletedAt should have been set")
+					}
+				} else {
+					assert.Nil(t, pld.CreatedAt, "CreatedAt should have not been set")
+					assert.Nil(t, pld.UpdatedAt, "UpdatedAt should have not been set")
+					assert.Nil(t, pld.DeletedAt, "DeletedAt should have not been set")
+					assert.Nil(t, pld.Organization.CreatedAt, "Organization.CreatedAt should have not been set")
+					assert.Nil(t, pld.Organization.UpdatedAt, "Organization.UpdatedAt should have not been set")
+					assert.Nil(t, pld.Organization.DeletedAt, "Organization.DeletedAt should have not been set")
+				}
+			}
+		})
+	}
+}
+
+func callGet(t *testing.T, params *experience.GetParams, auth *httptests.RequestAuth) *httptest.ResponseRecorder {
+	ri := &httptests.RequestInfo{
+		Endpoint: experience.Endpoints[experience.EndpointGet],
+		Params:   params,
+		Auth:     auth,
+	}
+
+	return httptests.NewRequest(t, ri)
+}
--- src/components/about/experience/testexperience/experience.go
+++ src/components/about/experience/testexperience/experience.go
@@ -1,7 +1,10 @@
 package testexperience
 
 import (
+	"testing"
+
 	"github.com/Nivl/go-rest-tools/storage/db"
+	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
 	"github.com/dchest/uniuri"
 	"github.com/melvin-laplanche/ml-api/src/components/about/experience"
 	"github.com/melvin-laplanche/ml-api/src/components/about/organizations/testorganizations"
@@ -23,4 +26,39 @@ func New() *experience.Experience {
 		OrganizationID: org.ID,
 		Organization:   org,
 	}
+}
+
+func NewPersisted(t *testing.T, dbCon db.DB, exp *experience.Experience) *experience.Experience {
+	if exp == nil {
+		exp = &experience.Experience{}
+	}
+
+	if exp.JobTitle == "" {
+		exp.JobTitle = uniuri.New()
+	}
+
+	if exp.Description == "" {
+		exp.Description = uniuri.New()
+	}
+
+	if exp.StartDate == nil {
+		exp.StartDate = db.Today()
+	}
+
+	if exp.Organization != nil && exp.OrganizationID == "" {
+		exp.OrganizationID = exp.Organization.ID
+	}
+
+	if exp.OrganizationID == "" {
+		org := testorganizations.NewPersisted(t, dbCon, nil)
+		exp.OrganizationID = org.ID
+		exp.Organization = org
+	}
+
+	if err := exp.Create(dbCon); err != nil {
+		t.Fatal(err)
+	}
+
+	lifecycle.SaveModels(t, exp)
+	return exp
 }
