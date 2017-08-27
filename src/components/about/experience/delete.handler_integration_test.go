ae18a2fab266e578760220920f624d7605ebf7f4
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-08-27 01:26:30 -0700

    Add missing import

--- src/components/about/experience/delete.handler_integration_test.go
+++ src/components/about/experience/delete.handler_integration_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Nivl/go-rest-tools/network/http/httptests"
 	"github.com/Nivl/go-rest-tools/security/auth/testauth"
 	"github.com/Nivl/go-rest-tools/storage/db"
+	"github.com/Nivl/go-rest-tools/types/apierror"
 	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
 	"github.com/melvin-laplanche/ml-api/src/components/about/experience"
 	"github.com/melvin-laplanche/ml-api/src/components/about/experience/testexperience"
