2e89e205a3e02360d288d3b41653cfd8f5df72d0
Melvin Laplanche <Nivl@users.noreply.github.com>
2017-09-04 17:50:13 -0700

    fix

--- src/components/about/organizations/delete.handler_integration_test.go
+++ src/components/about/organizations/delete.handler_integration_test.go
@@ -8,11 +8,11 @@ import (
 	"testing"
 
 	"github.com/Nivl/go-rest-tools/types/apierror"
+	"github.com/Nivl/go-rest-tools/types/datetime"
 
 	"github.com/Nivl/go-rest-tools/dependencies"
 	"github.com/Nivl/go-rest-tools/network/http/httptests"
 	"github.com/Nivl/go-rest-tools/security/auth/testauth"
-	"github.com/Nivl/go-rest-tools/storage/db"
 	"github.com/Nivl/go-rest-tools/types/models/lifecycle"
 	"github.com/melvin-laplanche/ml-api/src/components/about/organizations"
 	"github.com/melvin-laplanche/ml-api/src/components/about/organizations/testorganizations"
